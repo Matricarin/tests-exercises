5e403842eb2451412b74d5b37c2ee486fcc45ba7
matricarin <matricarin@gmail.com>
2026-06-21 12:29:35 +0500

    updated

--- Task003_KafkaBasicStructure/KafkaBasicStructureSolver.cs
+++ Task003_KafkaBasicStructure/KafkaBasicStructureSolver.cs
@@ -6,23 +6,24 @@ namespace Task003_KafkaBasicStructure;
 public static class KafkaBasicStructureSolver
 {
     private const string Topic = "orders-topic";
+    private const string ServiceName = "orders-processig-service";
     private const string ServersAddress = "some_server:9092";
 
-    public static async Task SendOrderMessageAsync(string orderId, string status)
+    public static async Task SendOrderMessageAsync(string orderId, string status, CancellationToken token)
     {
         var config = new ProducerConfig
         {
             BootstrapServers = ServersAddress
         };
 
-        var message = JsonDocument.Parse($"orderId:{orderId}, status:{status}");
+        var message = JsonSerializer.Serialize(new { OrderOd = orderId, Status = status });
 
         using (var producer = new ProducerBuilder<Null, string>(config).Build())
         {
             var result = await producer.ProduceAsync(Topic, new Message<Null, string>
             {
-                Value = message.ToString()
-            });
+                Value = message
+            }, token);
         }
     }
 
@@ -31,6 +32,7 @@ public static class KafkaBasicStructureSolver
         var config = new ConsumerConfig
         {
             BootstrapServers = ServersAddress,
+            GroupId = ServiceName,
             EnableAutoCommit = false
         };
 
@@ -43,6 +45,8 @@ public static class KafkaBasicStructureSolver
                 var consumeResult = consumer.Consume(token);
 
                 Console.WriteLine(consumeResult.Message.Value);
+
+                consumer.Commit(consumeResult);
             }
 
             consumer.Close();
