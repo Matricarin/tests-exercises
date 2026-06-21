c7790c20706bcf955e0c8033ab2f531cc180024b
matricarin <matricarin@gmail.com>
2026-06-21 11:59:24 +0500

    updated

---
+++ Task003_KafkaBasicStructure/KafkaBasicStructureSolver.cs
@@ -0,0 +1,19 @@
+﻿namespace Task003_KafkaBasicStructure
+{
+    public static class KafkaBasicStructureSolver
+    {
+        public static async Task SendOrderMessageAsync(string orderId, string status)
+        {
+            //  1. Сконфигурировать продьюсера
+
+            //  2. Обработать json
+
+            //  3. Отпрвить сообщение в "orders-topic"
+        }
+
+        public static void StartConsuming()
+        {
+            //  чтение из топика "orders-topic"
+        }
+    }
+}
