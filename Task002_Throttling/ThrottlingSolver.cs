1ff0fb459843240b6c3262cae6ea80734f63eac0
matricarin <matricarin@gmail.com>
2026-06-21 09:44:58 +0500

    updated

--- Task002_Throttling/ThrottlingSolver.cs
+++ Task002_Throttling/ThrottlingSolver.cs
@@ -1,10 +1,31 @@
-﻿namespace Task002_Throttling
-{
-    public sealed class ThrottlingSolver
-    {
-        public static async Task ProcessUrlsAsync(IEnumerable<string> urls, int maxParallelism)
-        {
+﻿namespace Task002_Throttling;
 
+public sealed class ThrottlingSolver
+{
+    public static async Task ProcessUrlsAsync
+    (
+        IEnumerable<string> urls,
+        int maxParallelism,
+        CancellationToken token
+    )
+    {
+        using var semaphore = new SemaphoreSlim(maxParallelism);
+
+        var tasks = urls.Select(s => ProcessUrl(s, semaphore, token));
+
+        await Task.WhenAll(tasks);
+    }
+
+    private static async Task ProcessUrl(string url, SemaphoreSlim semaphore, CancellationToken token)
+    {
+        try
+        {
+            await semaphore.WaitAsync(token);
+            await Task.Delay(url.Length + 1000, token);
+        }
+        finally
+        {
+            semaphore.Release();
         }
     }
 }
