797905b8b0eaab17959b0c9dd868d0ef518a18fd
matricarin <matricarin@gmail.com>
2026-06-21 12:18:35 +0500

    updated

--- Task003_KafkaBasicStructure/KafkaBasicStructureSolver.cs
+++ Task003_KafkaBasicStructure/KafkaBasicStructureSolver.cs
@@ -1,19 +1,51 @@
-﻿namespace Task003_KafkaBasicStructure
+﻿using System.Text.Json;
+using Confluent.Kafka;
+
+namespace Task003_KafkaBasicStructure;
+
+public static class KafkaBasicStructureSolver
 {
-    public static class KafkaBasicStructureSolver
+    private const string Topic = "orders-topic";
+    private const string ServersAddress = "some_server:9092";
+
+    public static async Task SendOrderMessageAsync(string orderId, string status)
     {
-        public static async Task SendOrderMessageAsync(string orderId, string status)
+        var config = new ProducerConfig
         {
-            //  1. Сконфигурировать продьюсера
+            BootstrapServers = ServersAddress
+        };
 
-            //  2. Обработать json
+        var message = JsonDocument.Parse($"orderId:{orderId}, status:{status}");
 
-            //  3. Отпрвить сообщение в "orders-topic"
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            var result = await producer.ProduceAsync(Topic, new Message<Null, string>
+            {
+                Value = message.ToString()
+            });
         }
+    }
 
-        public static void StartConsuming()
+    public static void StartConsuming(CancellationToken token)
+    {
+        var config = new ConsumerConfig
         {
-            //  чтение из топика "orders-topic"
+            BootstrapServers = ServersAddress,
+            EnableAutoCommit = false
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe(Topic);
+
+            while (!token.IsCancellationRequested)
+            {
+                var consumeResult = consumer.Consume(token);
+
+                Console.WriteLine(consumeResult.Message.Value);
+            }
+
+            consumer.Close();
         }
     }
 }
